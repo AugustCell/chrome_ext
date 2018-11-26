57c852e430664d9f625fd1cc1ce3942dac076318
Augusto Celis <augusto.celis@stonybrook.edu>
2018-11-26 14:25:13 -0500

    Changed icon and fixed history grabbing

--- compareReject.js
+++
@@ -1,5 +0,0 @@
-var url = window.locations.host;
-var test ="www.facebook.com";
-if(url.localeCompare(test) === 0){
-  console.log("This method worked!");
-}
--- historyGrab.js
+++ historyGrab.js
@@ -1,29 +1,22 @@
-//This history var is simply an empty array for the history vals
-var history = [];
-var historiesList = document.createElement('ul');
-
 //Server connection
 var HOST = 'wss://projectnickname123.herokuapp.com';
 var ws = new WebSocket(HOST);
 
+
+
+
+/*
 //This will actually get the results from history
-chrome.history.search({text: ""}, function(data) {
+ws.onopen = function(event){
+  chrome.history.search({text: ""}, function(data) {
     data.forEach(function(page) {
       var urli = page.url;
-      console.log(urli);
-      var lItem = document.createElement('li');
-      lItem.appendChild(document.createTextNode(urli));
-      historiesList.appendChild(lItem);
-      history[history.length] = urli;
+      chrome.storage.sync.get("id", function(ev){
+        var id=ev.id;
+        var jsonPackage = {id: id, type: 'history', history : urli};
+        ws.send(JSON.stringify(jsonPackage));
+      });
     });
-    document.getElementById('visitedWebpage').appendChild(historiesList);
-    var jsonPackage = {id: id, type: 'history', history : history};
-    ws.send(JSON.stringify(jsonPackage));
-});
-
-console.log(history);
-
-//chrome.browserAction.onClicked.addListener(function(tab){
-  //chrome.tabs.executeScript(tab.id, {file: 'compareReject.js', runAt: 'document_idle'}
-  //);
-//});
+  });
+}
+*/
